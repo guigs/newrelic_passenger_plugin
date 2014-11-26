aae60af5353f6698952db15f3f44790ce7444adc
guigs <guigs81@gmail.com>
2014-11-26 11:26:24 -0200

    Multiple app group support. Report CPU, number of processes, queue and sessions per app group; Remove last_used_time metric; Change agent GUID;

--- lib/status_parser.rb
+++ lib/status_parser.rb
@@ -1,7 +1,5 @@
 class Status
-  require 'date'
-
-  attr_reader :max, :current, :queue, :sessions, :cpu, :process_memory, :last_used_time
+  attr_reader :max, :current, :global_queue, :sessions, :cpu, :memory, :queue, :processes
 
   def initialize(output)
     status_command = output.match(/<.+/m).to_s
@@ -9,58 +7,69 @@ class Status
 
     @max = doc.xpath('//info/max').text.to_i
     @current = doc.xpath('//info/capacity_used').text.to_i
-    @queue = doc.xpath('//info/get_wait_list_size').text.to_i
+    @global_queue = doc.xpath('//info/get_wait_list_size').text.to_i
     @sessions = parse_sessions(doc)
     @cpu = parse_cpu(doc)
-    @process_memory = parse_app_memory(doc)
-    @last_used_time = parse_last_used(doc)
+    @memory = parse_memory(doc)
+    @queue = parse_queue(doc)
+    @processes = parse_processes(doc)
   end
 
   private
 
   def name_clean(app_name)
-    if app_name.length > 45
-      first_half = app_name.match('.+:').to_s
-      second_half = app_name.split('/')[-1].to_s
-      cleaned_name = first_half + ' ' + second_half
-    else
-      cleaned_name = app_name
-    end
-    cleaned_name
+    app_name.split('/').last.split('#').first
   end
 
   def parse_sessions(doc)
-    sessions_total = 0
-    doc.xpath('//process/sessions').each do |x|
-      sessions_total += x.text.to_i
+    groups = Hash.new(0)
+    doc.xpath('//group').each do |group|
+      group_name = name_clean(group.xpath('./name').text)
+      group.xpath('.//process/sessions').each do |x|
+        groups[group_name] += x.text.to_i
+      end
     end
-    sessions_total
+    groups
   end
 
   def parse_cpu(doc)
-    cpu_total = 0
-    doc.xpath('//process/cpu').each do |x|
-      cpu_total += x.text.to_f
+    groups = Hash.new(0)
+    doc.xpath('//group').each do |group|
+      group_name = name_clean(group.xpath('./name').text)
+      group.xpath('.//process/cpu').each do |x|
+        groups[group_name] += x.text.to_f
+      end
     end
-    cpu_total
+    groups
   end
 
-  def parse_app_memory(doc)
-    processes = Hash.new(0)
-    doc.xpath('//process').each do |x|
-      processes[name_clean(x.xpath('./command').text)] += x.xpath('./real_memory').text.to_i
+  def parse_memory(doc)
+    groups = Hash.new(0)
+    doc.xpath('//group').each do |group|
+      group_name = name_clean(group.xpath('./name').text)
+      group.xpath('.//process/real_memory').each do |x|
+        groups[group_name] += x.text.to_i
+      end
     end
-    processes
+    groups
   end
 
-  def parse_last_used(doc)
-    processes = Hash.new(0)
-    doc.xpath('//process').each_with_index do |x, index|
-      unix_stamp = (x.xpath('./last_used').text.to_i / 1000000)
-      elapsed = Time.now.to_i - unix_stamp
-      processes[(index + 1).to_s] = elapsed
+  def parse_queue(doc)
+    groups = Hash.new(0)
+    doc.xpath('//group').each do |group|
+      group_name = name_clean(group.xpath('./name').text)
+      groups[group_name] = group.xpath('./get_wait_list_size').text.to_i
     end
-    processes
+    groups
+  end
+
+  def parse_processes(doc)
+    groups = Hash.new(0)
+    doc.xpath('//group').each do |group|
+      group_name = name_clean(group.xpath('./name').text)
+      groups[group_name] = group.xpath('./capacity_used').text.to_i
+    end
+    groups
   end
 
 end
--- test/passenger_status_test.rb
+++ test/passenger_status_test.rb
@@ -14,27 +14,41 @@ class PassengerStatusTest < Test::Unit::TestCase
   end
 
   test "number of running passenger processes are parsed correctly" do
-    assert_equal 6, @status_parser.current
+    assert_equal 5, @status_parser.current
   end
 
   test "requests waiting on global queue are parsed correctly" do
-    assert_equal 3, @status_parser.queue
+    assert_equal 3, @status_parser.global_queue
   end
 
-  test "number of sessions (summed as total) parsed correctly" do
-    assert_equal 1, @status_parser.sessions
+  test "number of sessions by app group parsed correctly" do
+    assert_equal 2, @status_parser.sessions['test-app1']
+    assert_equal 0, @status_parser.sessions['test-app2']
+    assert_equal 1, @status_parser.sessions['test-app3']
   end
 
-  test "CPU (summed as total) parsed correctly" do
-    assert_equal 4.5, @status_parser.cpu
+  test "total CPU by app group parsed correctly" do
+    assert_equal 4.5, @status_parser.cpu['test-app1']
+    assert_equal 3, @status_parser.cpu['test-app2']
+    assert_equal 0, @status_parser.cpu['test-app3']
   end
 
-  test "memory by app parsed correctly" do
-    assert_equal 55620, @status_parser.process_memory['Passenger RackApp: testapp1']
+  test "total memory by group app parsed correctly" do
+    assert_equal 124736, @status_parser.memory['test-app1']
+    assert_equal 65228, @status_parser.memory['test-app2']
+    assert_equal 145212, @status_parser.memory['test-app3']
   end
 
-  test "time since last used parsed correctly" do
-    assert 5670 < @status_parser.last_used_time["1"]
+  test "queue by group app parsed correctly" do
+    assert_equal 2, @status_parser.queue['test-app1']
+    assert_equal 0, @status_parser.queue['test-app2']
+    assert_equal 1, @status_parser.queue['test-app3']
+  end
+
+  test "number of processes by group app parsed correctly" do
+    assert_equal 3, @status_parser.processes['test-app1']
+    assert_equal 1, @status_parser.processes['test-app2']
+    assert_equal 1, @status_parser.processes['test-app3']
   end
 
 end
